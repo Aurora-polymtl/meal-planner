9bc31e5f2bde955ebc06a00862af2d4c57384d38
Aurora <pascalelafond@hotmail.com>
2026-05-27 13:36:28 -0400

    Fix: Adding exemple for firebase config

--- src/app/firebase/firebase.config.ts
+++ src/app/firebase/firebase.config.ts
@@ -1,17 +1,7 @@
-// src/app/firebase/firebase.config.ts
-
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-
-const firebaseConfig = {
-  apiKey: "AIzaSyBoaN97PRO7QNe-ECIdma6pbzXbwwmHiI4",
-  authDomain: "meal-planner-9c0b6.firebaseapp.com",
-  projectId: "meal-planner-9c0b6",
-  storageBucket: "meal-planner-9c0b6.firebasestorage.app",
-  messagingSenderId: "24162187567",
-  appId: "1:24162187567:web:d0a64ad3552ae3e62789da"
-};
+import { firebaseConfig } from './firebase-secrets';
 
 const app = initializeApp(firebaseConfig);
 
